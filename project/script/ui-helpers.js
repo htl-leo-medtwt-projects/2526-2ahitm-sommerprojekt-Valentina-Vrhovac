eb01c72fee662cb01952795b9a8e6d4f4359006c
Valentina Vrhovac <v.vrhovac@students.htl-leonding.ac.at>
2026-04-20 22:35:55 +0200

    overflow fixed

--- project/script/ui-helpers.js
+++ project/script/ui-helpers.js
@@ -1,12 +1,26 @@
 function show(element) {
   if (element) {
     element.classList.remove("hidden");
+    if (element.id === "game-screen" || element.id === "menu-screen" || element.id === "settings-screen" || element.id === "loading-screen") {
+      document.body.style.overflow = "hidden";
+    }
   }
 }
 
 function hide(element) {
   if (element) {
     element.classList.add("hidden");
+    let gameScreen = document.getElementById("game-screen");
+    let menuScreen = document.getElementById("menu-screen");
+    let settingsScreen = document.getElementById("settings-screen");
+    let loadingScreen = document.getElementById("loading-screen");
+
+    if ((gameScreen && gameScreen.classList.contains("hidden")) &&
+      (menuScreen && menuScreen.classList.contains("hidden")) &&
+      (settingsScreen && settingsScreen.classList.contains("hidden")) &&
+      (loadingScreen && loadingScreen.classList.contains("hidden"))) {
+      document.body.style.overflow = "auto";
+    }
   }
 }
 
